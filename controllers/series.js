c54ad4ffbe09ebffbe82a173fd2a66585e79dc1d
Hugo Carvalho <hugosc@gmail.com>
2020-06-26 18:35:22 -0300

    Incluido função editar e excluir e listar

--- controllers/series.js
+++ controllers/series.js
@@ -1,20 +1,43 @@
+const labels = [
+    { id: 'to-watch', name: 'Para assistir' },
+    { id: 'to-watching', name: 'Assistindo' },
+    { id: 'watched', name: 'Assistido' },
+]
 
-
-const index = ({ Serie }, req, res) => {
-    Serie.find({}, (err, docs) => {
-        res.render('series/index', { series: docs })
-    })
+const index = async ({ Serie }, req, res) => {
+    const docs = await Serie.find({})
+    res.render('series/index', { series: docs, labels })
 }
-const novaProcess = ({ Serie }, req, res) => {
+const novaProcess = async ({ Serie }, req, res) => {
     const serie = new Serie(req.body)
-    serie.save(() => {
-        res.redirect('/series')
-    })       
+    await serie.save()
+    res.redirect('/series')
 }
 const novaForm = (req, res) => {
     res.render('series/nova')
 }
+const excluir = async ({ Serie }, req, res) => {
+    await Serie.remove({
+        _id: req.params.id
+    })
+    res.redirect('/series')
+}
+const editarProcess = async ({ Serie }, req, res) => {
+    Serie.findOne({ _id: req.params.id }, (err, serie) => {
+        serie.name = req.body.name
+        serie.status = req.body.status
+        serie.save()
+        res.redirect('/series')
+    })    
+}
+const editarForm = ({ Serie }, req, res) => {
+    
+    Serie.findOne({ _id: req.params.id }, (err, serie) => {
+        res.render('series/editar', { serie, labels })
+    })
+
+}
 
 module.exports = {
-    index, novaProcess, novaForm
+    index, novaProcess, novaForm, excluir, editarForm, editarProcess
 }
--- routes/series.js
+++ routes/series.js
@@ -10,4 +10,8 @@ router.get('/', seriesController.index.bind(null, models))
 router.get('/nova', seriesController.novaForm)
 router.post('/nova', seriesController.novaProcess.bind(null, models))
 
+router.get('/excluir/:id', seriesController.excluir.bind(null, models))
+router.get('/editar/:id', seriesController.editarForm.bind(null, models))
+router.post('/editar/:id', seriesController.editarProcess.bind(null, models))
+
 module.exports = router
